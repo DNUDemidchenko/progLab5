0906bac0f485a081a0731ba57d4d2f74ef1cede5
Alexey Demidchenko <alexey.demidchenko@gmail.com>
2022-12-29 12:39:14 +0200

    finished

--- progLab5/progLab5.cpp
+++ progLab5/progLab5.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+int count(int k)
+{
+    int i = 0;
+    do
+    {
+        i++;
+        k /= 10;
+    } while (k > 0);
+    return i;
+}
+
 int main()
 {
+    int n;
+    cout << "Part a\nInput natural number n:\n";
+    cin >> n;
+    double sum = 0;
+    for (int i = 1; i <= n; i++)
+        sum += count(i) / (i * i);
+    cout << "Result: " << sum << endl;
     
+    cout << "Part b\nInput natural number n:\n";
+    cin >> n;
+    double x;
+    cout << "Input real number x:\n";
+    cin >> x;
+    sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += pow(10, count(i)) / i * pow(1 - x, i);
+    }
+    cout << "Result: " << sum << endl;
 }
